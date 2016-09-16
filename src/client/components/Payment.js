27651d11cf90dfaf51e9d95bf301e5e44528c557
amanthapar <amanthapar@gmail.com>
2016-09-16 17:42:14 -0400

    simple payment boilerplate ok, need to add custom changes for each event

--- src/client/components/Payment.js
+++ src/client/components/Payment.js
@@ -2,7 +2,7 @@
 <script
   src="https://checkout.stripe.com/checkout.js" class="stripe-button"
   data-key="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-  data-amount="2000"
+  data-amount="1000"
   data-name="Cam.io"
   data-description="Sponsor the above event"
   data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
@@ -16,17 +16,20 @@
 //server side
 var stripe = require("stripe")("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
 
+app.post('/api/payment', function(req, res, next) {
 // Get the credit card details submitted by the form
-var token = request.body.stripeToken; // Using Express
+var token = req.body.stripeToken;
 
 // Create a charge: this will charge the user's card
 var charge = stripe.charges.create({
-  amount: 1000, // Amount in cents
+  amount: 1000,
   currency: "usd",
   source: token,
   description: "Example charge"
 }, function(err, charge) {
   if (err && err.type === 'StripeCardError') {
-    // The card has been declined
+
   }
+});
+
 });
