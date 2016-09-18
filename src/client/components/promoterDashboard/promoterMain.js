63613e744838b4a97f32d46d7657238247bc247f
John Fritz <johnfritz@Johns-Mac-4.local>
2016-09-17 17:05:39 -0700

    promoter data fetch is working

---
+++ src/client/components/promoterDashboard/promoterMain.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import PromoterLeft from './promoterLeft'
+
+
+export default class Promoters extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      firstName: '',
+      lastName: '',
+      eventsPromoting: []
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+  componentDidMount () {
+    const user = localStorage.getItem('username')
+    const url = `/user/${user}`
+
+    this.serverRequest = $.get(url, function (promoter) {
+      this.setState({
+        firstName: promoter.firstName,
+        lastName: promoter.lastName,
+        eventsPromoting: promoter.eventsPromoting
+      })
+    }.bind(this))
+  }
+
+  componentWillUnmount () {
+    this.serverRequest.abort()
+  }
+
+  render () {
+    return (
+      <PromoterLeft data={this.state} />
+     )
+  }
+}
