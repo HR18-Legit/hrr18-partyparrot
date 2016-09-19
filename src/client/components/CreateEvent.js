a4dd5c6f84b8e4af82ceca788695508855668d28
Graham Wimbrow <graham.wimbrow@gmail.com>
2016-09-19 19:48:04 -0400

    there's still a bug where the badge reward is not displayed for events that have been just created, and I don't really think it's worth any more time, so there

--- src/client/components/CreateEvent.js
+++ src/client/components/CreateEvent.js
@@ -36,9 +36,7 @@ export default class CreateEvent extends React.Component {
 
           <h2 className="h3-responsive">Search Event Titles and Locations</h2>
 
-          {/* begin realtime search */}
-
-          <form style={{ 'width':'100%' }}
+          <form className="col-xs-12"
                 onChange={ () => this.search(this.refs.keyword.value, this.refs.location.value) }>
 
             <input  className="inputEventInfo"
@@ -51,12 +49,11 @@ export default class CreateEvent extends React.Component {
                     ref="location" />
           </form>
 
-          {/* end realtime search */}
-
           <div className="row margin-top">
+
             <div className="col-xs-12">
               <hr />
-              <h3 className="h2-responsive">Selected Event:</h3>
+              <h3 className="h3-responsive">Selected Event:</h3>
               <input className="inputId" style={{ 'width':'75%' }} value={this.state.selectedEvent.name ? this.state.selectedEvent.name.html : ""} readOnly="true" ref="eventName"/>
               <div className="scrollBox margin-top text-xs-left">
                 <ul>
@@ -64,36 +61,41 @@ export default class CreateEvent extends React.Component {
                 </ul>
               </div>
             </div>
-            <div className="col-xs-12">
-              <hr />
-              <h2 className="h2-responsive">Prizes</h2>
-              <img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-gold_2x.png" alt="" />
-              <input className="inputEventInfo" placeholder="Points to earn.." style={{ 'margin':"25px" }} ref ={ (input) => this.gPoint = input } />
-              <input className="inputEventInfo" placeholder="Reward..." style={{ 'marginTop':"25px" }} ref ={ (input) => this.gReward = input }/>
-              <div />
-              <img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-silver_2x.png" alt="" />
-              <input className="inputEventInfo" placeholder="Points to earn.." style={{ 'margin':"25px" }} ref ={ (input) => this.sPoint = input }/>
-              <input className="inputEventInfo" placeholder="Reward..." style={{ 'marginTop':"25px" }} ref ={ (input) => this.sReward = input }/>
-              <div />
-              <img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-bronze_2x.png" alt="" />
-              <input className="inputEventInfo" placeholder="Points to earn.." style={{ 'margin':"25px" }} ref ={ (input) => this.bPoint = input }/>
-              <input className="inputEventInfo" placeholder="Reward..." style={{ 'marginTop':"25px" }} ref ={ (input) => this.bReward = input }/>
-            </div>
-            <div className="col-xs-12">
-              <hr />
-              <h2 className="h2-responsive">Post your Event</h2>
-              <button className="btn btn-lg btn-default waves-effect waves-light" onClick={() => this.handleSubmit({
-                userEmail: localStorage.username,
-                name: this.state.selectedEvent.name.html,
-                gPoint: this.gPoint.value,
-                gReward: this.gReward.value,
-                sPoint: this.sPoint.value,
-                sReward: this.sReward.value,
-                bPoint: this.bPoint.value,
-                bReward: this.bReward.value,
-                eventbrite: this.state.selectedEvent
-              })}>Submit</button>
-            </div>
+
+            <h2 className="h2-responsive">Prizes</h2>
+            <p className="p-responsive">Tap into crowdsourced promotions! CAM.IO adds incentives to <a href="https://www.eventbrite.com/">Eventbrite</a>, using <a href="https://bitly.com/">Bitly</a> to track your promoters' link totals.</p>
+
+            <table  style={{"width":"75%","margin":"0 auto"}}>
+              <tbody>
+                <tr>
+                  <td><img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-gold_2x.png"/></td>
+                  <td><input type="number" min="0" step="1" placeholder="Clicks" required style={{"textAlign":"center"}} ref ={ (input) => this.gPoint = input }/></td>
+                  <td><input type="text" placeholder="Reward" required style={{"textAlign":"center"}} ref ={ (input) => this.gReward = input }/></td>
+                </tr>
+                <tr>
+                  <td><img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-silver_2x.png" alt="" /></td>
+                  <td><input type="number" min="0" step="1" placeholder="Clicks" required style={{"textAlign":"center"}} ref ={ (input) => this.sPoint = input }/></td>
+                  <td><input type="text" placeholder="Reward" required style={{"textAlign":"center"}} ref ={ (input) => this.sReward = input }/></td>
+                </tr>
+                <tr>
+                  <td><img style={{ "width":"90px", 'borderRight':'1px solid rgba(0,0,0,.1)', 'padding':'20px' }} src="http://ssl.gstatic.com/onebox/sports/olympics/2016/medals2/ic_medal-large-bronze_2x.png"/></td>
+                  <td><input type="number" min="0" step="1" placeholder="Clicks" required style={{"textAlign":"center"}} ref ={ (input) => this.bPoint = input }/></td>
+                  <td><input type="text" placeholder="Reward" required style={{"textAlign":"center"}} ref ={ (input) => this.bReward = input }/></td>
+                </tr>
+              </tbody>
+            </table>
+
+            <button className="btn btn-lg btn-default waves-effect waves-light" onClick={() => this.handleSubmit({
+              userEmail: localStorage.username,
+              name: this.state.selectedEvent.name.html,
+              gPoint: this.gPoint.value,
+              gReward: this.gReward.value,
+              sPoint: this.sPoint.value,
+              sReward: this.sReward.value,
+              bPoint: this.bPoint.value,
+              bReward: this.bReward.value,
+              eventbrite: this.state.selectedEvent
+            })}>Submit</button>
           </div>
         </div>
       </div>
@@ -127,7 +129,7 @@ export default class CreateEvent extends React.Component {
   }
 
   search(keyword, location){
-    var url = `https://www.eventbriteapi.com/v3/events/search/?q=${keyword}&sort_by=best&location.address=${location}&token=YZO3HZ5MJZYKY6QU64H2`;
+    var url = `https://www.eventbriteapi.com/v3/events/search/?q=${keyword}&sort_by=best&location.address=${location}&token=7RMWHXP6CK2F2HSP6OSI`;
     Request.get(url).then((response) => {
       this.setState({
         events: response.body.events
@@ -136,7 +138,7 @@ export default class CreateEvent extends React.Component {
   }
 
   searchId(id){
-    var url = `https://www.eventbriteapi.com/v3/events/${id}/?token=YZO3HZ5MJZYKY6QU64H2`;
+    var url = `https://www.eventbriteapi.com/v3/events/${id}/?token=7RMWHXP6CK2F2HSP6OSI`;
     Request.get(url).then((response) => {
       this.setState({
         event: response.body
